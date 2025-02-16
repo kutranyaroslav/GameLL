8f4fd4a662daf429fe1b04ee41b2ab5c243870ad
kutranyaroslav <kutranaroslav@gmail.com>
2025-02-16 12:32:23 +0100

    Entity Manager Header File

--- GameLL/Player.cpp
+++ GameLL/Player.cpp
@@ -42,9 +42,6 @@ void Player::OnEntityCollision(EntityBase* i_collider, bool i_attack) {
 }
 
 void Player::React(EventDetails* i_details) {
-	//ticket number 7
-	//easy as fuck regarding the ticker by adding callback in costructor give through parameters event
-	//details where u will change the key_code change the value of it then switch of ifs for moving and done
 	if (i_details->m_name == "Player_Moveright") {
 		Move(Direction::Right);
 	}
