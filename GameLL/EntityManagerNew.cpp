18a438991204c6e45eeb7ca7746d53bbdfa675dc
kutranyaroslav <kutranaroslav@gmail.com>
2025-02-22 20:44:56 +0100

    finalising entity manager

---
+++ GameLL/EntityManagerNew.cpp
@@ -0,0 +1,132 @@
+#include "EntityManagerNew.h"
+
+
+EntityManagerNew::EntityManagerNew(SystemManager* i_systemMgr, TextureManager* i_textureMgr):
+	m_systems(i_systemMgr), m_textureManager(i_textureMgr), m_idCounter(0)
+{
+	AddComponentType<C_Position>(Component::Position);
+	/*AddComponentType<C_SpriteSheet>(Component::SpriteSheet);
+	AddComponentType<C_State>(Component::State);
+	AddComponentType<C_Movable>(Component::Movable);
+	AddComponentType<C_Controller>(Component::Controller);
+	AddComponentType <C_Collidable> (Component::Collidable);*/
+}
+
+EntityManagerNew::~EntityManagerNew() { Purge(); }
+
+int EntityManagerNew::AddEntity(const Bitmask& i_mask) {
+	unsigned int entity = m_idCounter;
+	if (!m_entities.emplace(entity, EntityData(0, ComponentContainer())).second) { return -1; }
+	++m_idCounter;
+	for (unsigned int i = 0; i < N_COMPONENT_TYPES; ++i) {
+		if (i_mask.getBit(i)) { AddComponent(entity, (Component)i); }
+	}
+	/*m_systems->EntityModified(entity, i_mask);
+	m_systems->addEvent(entity, (EventId)EntityEvent::Spawned);*/
+	return entity;
+}
+int EntityManagerNew::AddEntity(const std::string& i_file) {
+	int EntityId = -1;
+	std::ifstream file;
+	file.open(Utils::GetWorkingDirectory() + "media/Entities/" + i_file + ".entity");
+	if (!file.is_open()) {
+		return -1;
+	}
+	std::string line;
+	while (std::getline(file, line)) {
+		if (line[0] == '|') { continue; }
+		std::stringstream keystream(line);
+		std::string type; 
+		keystream >> type;
+		if (type == "Name") {
+
+		}
+		else if (type == "Attributes") {
+			if (EntityId == -1) { continue; }
+			Bitset set = 0;
+			Bitmask mask;
+			keystream >> set;
+			mask.setMask(set);
+			EntityId = AddEntity(mask);
+			if (EntityId == -1) { return -1; }
+		}
+		else if (type == "Component") {
+			if (EntityId == -1) { continue; }
+			unsigned int c_id = 0;
+			keystream >> c_id;
+			C_Base* component = GetComponent<C_Base>(EntityId,(Component) c_id);
+			if (!component) { continue; }
+			keystream >> *component;
+			if (component->getComponentType() == Component::SpriteSheet) {
+				/*C_SpriteSheet* sheet = (C_SpriteSheet*)component;
+				sheet->Create(m_textureManager);*/
+			}
+		}
+	}
+	file.close();
+	return EntityId;
+}
+
+bool EntityManagerNew::RemoveEntity(const EntityId& i_id) {
+	auto itr = m_entities.find(i_id);
+	if (itr == m_entities.end()) { return false; }
+	while (itr->second.second.begin() != itr->second.second.end()) {
+		delete itr->second.second.back();
+		itr->second.second.pop_back();
+		
+	}
+	m_entities.erase(itr);
+	//m_systems->RemoveEntity(i_id);
+	return true;
+}
+bool EntityManagerNew::AddComponent(const EntityId& i_id, const Component& i_component) {
+	auto itr = m_entities.find(i_id);
+	if (itr == m_entities.end()) { return false; }
+	if (itr->second.first.getBit((unsigned int)i_component)) {
+		return false;
+	}
+	auto itr2 = m_cFactory.find(i_component);
+	if (itr2 == m_cFactory.end()) { return false; }
+	C_Base* component = itr2->second();
+	itr->second.second.emplace_back(component);
+	itr->second.first.turnOnBit((unsigned int) i_component);
+	//m_systems->EntityModified(i_id, itr->second.first);
+	return true;
+
+}
+
+bool EntityManagerNew::RemoveComponent(const EntityId& i_id, const Component& i_component) {
+	auto itr = m_entities.find(i_id);
+	if (itr == m_entities.end()) { return false; }
+	if (!itr->second.first.getBit((unsigned int)i_component)) {
+		return false;
+	}
+	auto& container = itr->second.second;
+	auto component = std::find_if(container.begin(), container.end(), [&i_component](C_Base* c ) {
+		return c->getComponentType() == i_component;
+		});
+	if (component == container.end()) { return false; }
+	delete (* component);
+	container.erase(component);
+	itr->second.first.clearBit((unsigned int )i_component);
+	//m_systems->EntityModified(i_id, itr->second.first);
+	return true;
+
+}
+bool EntityManagerNew::HasComponent(const EntityId& i_id, const Component& i_component) {
+	auto itr = m_entities.find(i_id);
+	if (itr == m_entities.end()) { return false; }
+	return itr->second.first.getBit((unsigned int)i_component);
+}
+
+void EntityManagerNew::Purge() {
+	//m_systems->PurgeEntities();
+	for (auto& entity : m_entities) {
+		for (auto& component : entity.second.second) { delete component; }
+		entity.second.second.clear();
+		entity.second.first.Clear();
+	}
+	m_entities.clear();
+	m_idCounter = 0;
+}
+
--- GameLL/EntityManagerNew.h
+++ GameLL/EntityManagerNew.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Bitmask.h"
 #include "C_Base.h"
+#include "C_Position.h"
 #include "TextureManager.h"
 #include <vector>
 #include <string>
@@ -23,6 +24,7 @@ public:
 	int AddEntity(const std::string& i_file);
 	bool RemoveEntity(const EntityId& i_id);
 	bool AddComponent(const EntityId& i_id, const Component& i_component);
+	bool RemoveComponent(const EntityId& i_id, const Component& i_component);
 	bool HasComponent(const EntityId& i_id, const Component& i_component);
 
 	void Purge();
@@ -31,6 +33,22 @@ private:
 	template <class T>
 	void AddComponentType(const Component& i_id) {
 		m_cFactory[i_id] = []()->C_Base* {return new T();};
+	}
+
+	template<class T>
+	T* GetComponent(const EntityId& i_entityId, const Component& i_component) {
+		auto itr = m_entities.find(i_entityId);
+		if (itr == m_entities.end()) { return nullptr; }
+		if(!itr->second.first.getBit((unsigned int)i_component))
+		{
+			return nullptr;
+		}
+		auto& container = itr->second.second;
+		auto component = std::find_if(container.begin(), container.end(), [&i_component](C_Base* c) {
+			return c->getComponentType() == i_component;
+		});
+
+		return (component != container.end() ? dynamic_cast <T*> (*component) : nullptr);
 	}
 	unsigned int m_idCounter;
 	EntityContainer m_entities;
